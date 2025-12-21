59c46b2a5a00e07c6f4a10a0542e4d232fb079c3
SPaul <stanescupaul056@gmail.com>
2025-12-21 23:25:07 +0200

    modificari ui/ux pentru home screen

--- my-app/src/routes/Home/Home.jsx
+++ my-app/src/routes/Home/Home.jsx
@@ -4,14 +4,28 @@ import styles from "./Home.module.css";
 export function Home() {
   return (
     <div className={styles.container}>
-      <h1>MedicaBase</h1>
-      <div className={styles.buttonBody}>
+      <div className={styles.topTab}>
         <Link to="/doctors/login">
           <button className={styles.buttonLoginStyle}>Login</button>
         </Link>
-        <Link to="/patients/login">
-          <button className={styles.buttonPatientStyle}>Patients</button>
-        </Link>
+      </div>
+      <div className={styles.bottomTab}>
+        <div className={styles.titleLable}>
+          <p className={styles.titleStyle}>
+            Create your own medical base to store your patients allergies with
+            MedicaBase or see your allergies as a patient.
+          </p>
+        </div>
+        <div className={styles.buttonLable}>
+          <Link to="/doctors/register">
+            <button className={styles.buttonRegisterStyle}>
+              Create account
+            </button>
+          </Link>
+          <Link to="/patients/login">
+            <button className={styles.buttonPatientStyle}>Patients</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
